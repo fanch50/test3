7521a1726718892615ce197e67471fc296d536ee
fanch50 <83545632+fanch50@users.noreply.github.com>
2021-07-22 22:10:57 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,38 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Diamond)
-    basic.showIcon(IconNames.Target)
-    basic.showIcon(IconNames.SmallDiamond)
     soundExpression.giggle.play()
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.SmallDiamond)
+    }
+    basic.clearScreen()
 })
 input.onButtonPressed(Button.AB, function () {
+    soundExpression.twinkle.play()
     basic.showString("" + (input.temperature()))
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showLeds(`
-        . # # # .
-        . # # # .
-        . . # . .
-        # . # . #
-        # # # # #
-        `)
-    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+    music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Square)
+        basic.showIcon(IconNames.SmallSquare)
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showIcon(IconNames.SmallSquare)
+    }
+    basic.clearScreen()
 })
 basic.forever(function () {
 	
